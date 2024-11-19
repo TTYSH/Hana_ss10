a8fe9a580b1d55dbda1e412ab9a182ca21537414
TTYSH <trang123123100@gamil.com>
2024-11-19 08:22:27 +0700

    edit_ss10

--- config/database.config.js
+++ config/database.config.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 const connectDB = async () => {
     try {
-        await mongoose.connect('mongodb://localhost:27017/Nodejs');
+        await mongoose.connect('mongodb+srv://phtranga23049:phtranga23049@cluster0.4ovno.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0');
         console.log('Kết nối MongoDB thành công');
     } catch (err) {
         console.error('Lỗi kết nối MongoDB:', err);
